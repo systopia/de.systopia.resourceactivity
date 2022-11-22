1cb25fcf0196e6c9c4226f9d547402bb991dda2a
Jens Schuppe <schuppe@systopia.de>
2022-11-22 13:39:13 +0100

    Synchronise managed entities

--- resourceactivity.php
+++ resourceactivity.php
@@ -16,6 +16,7 @@
 require_once 'resourceactivity.civix.php';
 // phpcs:disable
 use CRM_Resourceactivity_ExtensionUtil as E;
+use Civi\Api4\Managed;
 // phpcs:enable
 
 /**
@@ -43,6 +44,27 @@ function resourceactivity_civicrm_install(): void {
  */
 function resourceactivity_civicrm_postInstall(): void {
   _resourceactivity_civix_civicrm_postInstall();
+
+  // Reconcile managed entities again for our custom group to pick up the
+  // correct activity type to be attached to.
+  Managed::reconcile(FALSE)
+    ->addModule(E::LONG_NAME)
+    ->execute();
+
+  // Add a foreign key constraint to the custom field, allowing only resource
+  // demand IDs as values.
+  CRM_Core_DAO::singleValueQuery("
+  ALTER TABLE civicrm_value_activity_resource_information
+      MODIFY resource_demand INT UNSIGNED DEFAULT NULL,
+      ADD CONSTRAINT FK_civicrm_value_activity_resource_information_resource_demand FOREIGN KEY (resource_demand)
+          REFERENCES civicrm_resource_demand(id);
+  ");
+  // Mark the managed entity as modified to prevent it from being reset.
+  Api4\Managed::update(FALSE)
+    ->addWhere('module', '=', E::LONG_NAME)
+    ->addWhere('name', '=', 'CustomField__activity_resource_information__resource_demand')
+    ->addValue('entity_modified_date', date('Y-m-d H:i:s'))
+    ->execute();
 }
 
 /**
@@ -90,4 +112,91 @@ function resourceactivity_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  */
 function resourceactivity_civicrm_entityTypes(&$entityTypes): void {
   _resourceactivity_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function resourceactivity_civicrm_managed(&$entities) {
+  _resourceactivity_civix_civicrm_managed($entities);
+
+  // Synchronise activity type.
+  $entities[] = [
+    'module' => E::LONG_NAME,
+    'name' => 'OptionValue__activity_type__resource_assignment',
+    'entity' => 'OptionValue',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'option_group_id.name' => 'activity_type',
+        'name' => 'resource_assignment',
+        'label' => 'Resource Assignment',
+        'description' => E::ts('CiviResource Activity resource assignments'),
+        'is_reserved' => TRUE,
+        'is_active' => TRUE,
+        'icon' => 'fa-tasks',
+      ],
+    ],
+    'match' => ['option_group_id.name', 'name'],
+  ];
+
+  // Synchronise custom group on activity objects.
+  $entities[] = [
+    'module' => E::LONG_NAME,
+    'name' => 'CustomGroup__activity_resource_information',
+    'entity' => 'CustomGroup',
+    'cleanup' => 'always',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'activity_resource_information',
+        'title' => 'Activity Resource Information',
+        'extends' => 'Activity',
+        'extends_entity_column_value' => Api4\OptionValue::get(FALSE)
+          ->addSelect('value')
+          ->addWhere('option_group_id:name', '=', 'activity_type')
+          ->addWhere('name', '=', 'resource_assignment')
+          ->execute()
+          ->column('value'),
+        // Note: "is_reserved" hides the custom field group in the UI.
+        'is_reserved' => 1,
+        'table_name' => 'civicrm_value_activity_resource_information',
+      ],
+    ],
+    'match' => ['name'],
+  ];
+
+  // Synchronise custom field for storing resource demand on activity objects.
+  $entities[] = [
+    'module' => E::LONG_NAME,
+    'name' => 'CustomField__activity_resource_information__resource_demand',
+    'entity' => 'CustomField',
+    'cleanup' => 'always',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'resource_demand',
+        'label' => 'Resource Demand',
+        'custom_group_id.name' => 'activity_resource_information',
+        'html_type' => 'Text',
+        'data_type' => 'Int',
+        'is_required' => 1,
+        'is_searchable' => 0,
+        'is_search_range' => 0,
+        'is_view' => 1,
+        'in_selector' => 0,
+        'column_name' => 'resource_demand'
+      ],
+    ],
+    'match' => ['custom_group_id.name', 'name'],
+  ];
 }
